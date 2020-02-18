1cd9716121f06b071075121316a7f308a8e5c21d
Jack Lin <jlin@castlighthealth.com>
2020-02-17 21:56:54 -0800

    modifying queryset to consider division of organization

--- attendees/occasions/views/participations.py
+++ attendees/occasions/views/participations.py
@@ -9,10 +9,11 @@ from attendees.occasions.models import Participation
 
 
 @method_decorator([login_required], name='dispatch')
-class ParticipationListView(ListView):
-    queryset = Participation.objects.select_related('character', 'team', 'attending', 'attending__attendee').order_by('gathering__meet', 'gathering__start')
+class ParticipationListView(ListView): #.filter(attending__divisions__id__in=[3]) needs to be replaced with request.user.attended_divisions_slugs, and also need .filter(gathering__start__gte=timezone.now(),)
+    queryset = Participation.objects.select_related('character', 'team', 'attending', 'gathering', 'attending__attendee').filter(attending__divisions__slug__in=['children_ministry']).order_by('gathering__meet', '-gathering__start')
     template_name = 'occasions/participations/index.html'
     paginate_by = 500
+
 
 participation_list_view = ParticipationListView.as_view()
 
