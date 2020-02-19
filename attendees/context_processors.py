cd832f68d87df2292944c813c9384dae84247bb0
Jack Lin <jlin@castlighthealth.com>
2020-02-18 22:15:12 -0800

    add organization_slug from user

--- attendees/context_processors.py
+++ attendees/context_processors.py
@@ -9,4 +9,5 @@ def common_variables(request):
     return {
         'timezone_name': datetime.now(timezone(parse.unquote(tzname))).tzname(),
         'user_organization_name': request.user.organization.display_name if request.user.organization else '',
+        'user_organization_name_slug': request.user.organization.slug if request.user.organization else '0_organization_slug',
     }
