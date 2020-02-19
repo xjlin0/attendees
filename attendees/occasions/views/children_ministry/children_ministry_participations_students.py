2872c8f8a3f04a88c8181b114c1c9bfa3e383284
Jack Lin <jlin@castlighthealth.com>
2020-02-18 22:07:40 -0800

    arrange view and template files

--- attendees/occasions/views/children_ministry/children_ministry_participations_students.py
+++ attendees/occasions/views/children_ministry/children_ministry_participations_students.py
@@ -8,7 +8,7 @@ from attendees.occasions.models import Participation
 @method_decorator([login_required], name='dispatch')
 class ChildrenMinistryParticipationStudentListView(ListView): #.filter(attending__divisions__id__in=[3]) needs to be replaced with request.user.attended_divisions_slugs, and also need .filter(gathering__start__gte=timezone.now(),)
     queryset = Participation.objects.select_related('character', 'team', 'attending', 'gathering', 'attending__attendee').filter(attending__divisions__slug__in=['children_ministry'], character__slug='student').order_by('gathering__meet', '-gathering__start', 'character__display_order')
-    template_name = 'occasions/participations/student_index.html'
+    template_name = 'occasions/children_ministry/participations/student_index.html'
     paginate_by = 500
 
 
