3c86f34296eaafdd89cab9fbd78b6b97126f6f45
Jack Lin <jlin@castlighthealth.com>
2020-02-21 10:03:02 -0800

    got path params in JSONResponseMixin but cannot get url params there

--- attendees/occasions/views/children_ministry/children_ministry_participations_leaders.py
+++ attendees/occasions/views/children_ministry/children_ministry_participations_leaders.py
@@ -6,22 +6,28 @@ from django.core import serializers
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
 from datetime import timedelta
+import logging
+
 
 from attendees.occasions.models import Participation
 
+logger = logging.getLogger(__name__)
+class ParticipationJSONResponseMixin:
 
-class JSONResponseMixin:
     """
     A mixin that can be used to render a JSON response.
     https://docs.djangoproject.com/en/3.0/topics/class-based-views/mixins/#more-than-just-html
     """
-    def render_to_json_response(self, context, **response_kwargs):
+    def render_to_json_response(self, context, params, **response_kwargs):
         """
         Returns a JSON response, transforming 'context' to make the payload.
         filter(attending__divisions__id__in=[3]) needs to be replaced with request.user.attended_divisions_slugs, and also need .filter(gathering__start__gte=timezone.now(),)
         """
         participations = Participation.objects.select_related('character', 'team', 'attending', 'gathering', 'attending__attendee').filter(attending__divisions__slug__in=['children_ministry']).exclude(character__slug='student').order_by('gathering__meet', '-gathering__start', 'character__display_order')
-
+        logger.info("27 The value of context is %s", context)
+        logger.info("28 The value of response_kwargs is %s", response_kwargs)
+        logger.info("29 The value of params is %s", params)
+        # logger.info("29 The value of request.GET('hi') is %s", 5)
         return JsonResponse(
             serializers.serialize('json', participations),
             safe=False,
@@ -30,14 +36,16 @@ class JSONResponseMixin:
 
 
 @method_decorator([login_required], name='dispatch')
-class ChildrenMinistryParticipationLeaderListView(ListView, JSONResponseMixin):
+class ChildrenMinistryParticipationLeaderListView(ListView, ParticipationJSONResponseMixin):
     queryset = []
     template_name = 'occasions/children_ministry/participations/leader_index.html'
 
     def render_to_response(self, context, **kwargs):
-        # Look for a 'format=json' GET argument
+        logger.info("44 The value of kwargs is %s", kwargs)
+        logger.info("45 The value of self.request is %s", self.request)
+        logger.info("46 The value of self.kwargs is %s", self.kwargs)
         if self.request.is_ajax():
-            return self.render_to_json_response(context, **kwargs)
+            return self.render_to_json_response(context, params=self.kwargs, **kwargs)
         else:
             return super().render_to_response(context, **kwargs)
 
