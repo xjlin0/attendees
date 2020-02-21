ced1a9e4c49d172c8e50002bf6643bb91e1761a5
Jack Lin <jlin@castlighthealth.com>
2020-02-21 08:39:42 -0800

    finally ajax return correct data from server

--- attendees/context_processors.py
+++ attendees/context_processors.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from urllib import parse
 
 
-def common_variables(request): #TODO move organization info to view
+def common_variables(request):  # TODO move organization info to view
     tzname = request.COOKIES.get('timezone') or settings.CLIENT_DEFAULT_TIME_ZONE
     user_organization_name = settings.PROJECT_NAME
     user_organization_name_slug = '0_organization_slug'
@@ -16,4 +16,4 @@ def common_variables(request): #TODO move organization info to view
         'timezone_name': datetime.now(timezone(parse.unquote(tzname))).tzname(),
         'user_organization_name': user_organization_name,
         'user_organization_name_slug': user_organization_name_slug,
-    }
+    }  # Todo pass in user_division_slugs based on users' auth_group to display menu
--- attendees/occasions/views/children_ministry/children_ministry_participations_leaders.py
+++ attendees/occasions/views/children_ministry/children_ministry_participations_leaders.py
@@ -1,6 +1,9 @@
 from django.views.generic.list import ListView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
 from datetime import timedelta
@@ -8,11 +11,51 @@ from datetime import timedelta
 from attendees.occasions.models import Participation
 
 
+class JSONResponseMixin:
+    """
+    A mixin that can be used to render a JSON response.
+    https://docs.djangoproject.com/en/3.0/topics/class-based-views/mixins/#more-than-just-html
+    """
+    def render_to_json_response(self, context, **response_kwargs):
+        """
+        Returns a JSON response, transforming 'context' to make the payload.
+        """
+        participations = Participation.objects.select_related('character', 'team', 'attending', 'gathering', 'attending__attendee').filter(attending__divisions__slug__in=['children_ministry']).exclude(character__slug='student').order_by('gathering__meet', '-gathering__start', 'character__display_order')
+
+        return JsonResponse(
+            serializers.serialize('json', participations), safe=False,
+            **response_kwargs
+        )
+
+
 @method_decorator([login_required], name='dispatch')
-class ChildrenMinistryParticipationLeaderListView(ListView): #.filter(attending__divisions__id__in=[3]) needs to be replaced with request.user.attended_divisions_slugs, and also need .filter(gathering__start__gte=timezone.now(),)
-    queryset = Participation.objects.select_related('character', 'team', 'attending', 'gathering', 'attending__attendee').filter(attending__divisions__slug__in=['children_ministry']).exclude(character__slug='student').order_by('gathering__meet', '-gathering__start', 'character__display_order')
+class ChildrenMinistryParticipationLeaderListView(ListView, JSONResponseMixin): #.filter(attending__divisions__id__in=[3]) needs to be replaced with request.user.attended_divisions_slugs, and also need .filter(gathering__start__gte=timezone.now(),)
+    # queryset = Participation.objects.select_related('character', 'team', 'attending', 'gathering', 'attending__attendee').filter(attending__divisions__slug__in=['children_ministry']).exclude(character__slug='student').order_by('gathering__meet', '-gathering__start', 'character__display_order') #use .prefetch_related()for M2M
+    # model = Participation
     template_name = 'occasions/children_ministry/participations/leader_index.html'
     paginate_by = 500
+
+    def get_queryset(self):
+        # sss= self.ajax_list_partial #https://dkoug.com/post/django-ajax-class-based-views/
+        meet_slug = self.kwargs.get('meet_slug', None)
+        assembly_slug = self.kwargs.get('assembly_slug', None)
+        user_organization = self.request.user.organization or None
+        url_division_slug = self.kwargs.get('division_slug', None)
+        if user_organization and url_division_slug and meet_slug and assembly_slug:
+            user_division_slugs = user_organization.division_set.values_list('slug', flat=True)
+            # check if passed in url_division_slug belongs user's organization's divisions
+            # then filter Participations baed on division/meet/assembly slugs
+            # how to return json?
+            return Participation.objects.select_related('character', 'team', 'attending', 'gathering', 'attending__attendee').filter(attending__divisions__slug__in=['children_ministry']).exclude(character__slug='student').order_by('gathering__meet', '-gathering__start', 'character__display_order')
+        else: # no url params, prepared empty table for ajax, probably TemplateResponseMixin or JSONResponseMixin?
+            return []
+
+    def render_to_response(self, context):
+        # Look for a 'format=json' GET argument
+        if self.request.is_ajax():
+            return self.render_to_json_response(context)
+        else:
+            return super().render_to_response(context)
 
 
 children_ministry_participation_leader_list_view = ChildrenMinistryParticipationLeaderListView.as_view()
