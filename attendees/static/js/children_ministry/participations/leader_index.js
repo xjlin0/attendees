ca9774e0d9d3af6cef123620a3c896a86daa60f7
Jack Lin <jlin@castlighthealth.com>
2020-02-21 19:36:45 -0800

    make js loading work

---
+++ attendees/static/js/children_ministry/participations/leader_index.js
@@ -0,0 +1,9 @@
+Attendees.leaderIndex = {
+  init: () => {
+    console.log("attendees/static/js/children_ministry/participations/leader_index.js");
+  },
+}
+
+$(document).ready(() => {
+  Attendees.leaderIndex.init();
+});
--- attendees/static/js/shared/base.js
+++ attendees/static/js/shared/base.js
@@ -1,6 +1,6 @@
 (($, Attendee) => {
-  if (typeof Attendee === 'undefined') window.Attendee = {};
-  console.log("attendees/attendees/static/js/shared/base.js");
+  if (typeof Attendees === 'undefined') window.Attendees = {};
+  console.log("attendees/static/js/shared/base.js");
   const timeZoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
   document.cookie = 'timezone=' + encodeURIComponent(timeZoneName) + '; path=/';
 })(window.jQuery, window.Attendee); // https://stackoverflow.com/a/18315393
