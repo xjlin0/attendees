70f8372f5bd51cdc0c00b3265dd41638577cd8cf
Jack Lin <jlin@castlighthealth.com>
2020-02-21 20:13:59 -0800

    both search params and url params now available in ParticipationJSONResponseMixin

--- attendees/occasions/views/children_ministry/children_ministry_participations_leaders.py
+++ attendees/occasions/views/children_ministry/children_ministry_participations_leaders.py
@@ -12,22 +12,25 @@ import logging
 from attendees.occasions.models import Participation
 
 logger = logging.getLogger(__name__)
+
+
 class ParticipationJSONResponseMixin:
 
     """
     A mixin that can be used to render a JSON response.
     https://docs.djangoproject.com/en/3.0/topics/class-based-views/mixins/#more-than-just-html
     """
-    def render_to_json_response(self, context, params, **response_kwargs):
+    def render_to_json_response(self, context, **response_kwargs):
         """
         Returns a JSON response, transforming 'context' to make the payload.
         filter(attending__divisions__id__in=[3]) needs to be replaced with request.user.attended_divisions_slugs, and also need .filter(gathering__start__gte=timezone.now(),)
         """
         participations = Participation.objects.select_related('character', 'team', 'attending', 'gathering', 'attending__attendee').filter(attending__divisions__slug__in=['children_ministry']).exclude(character__slug='student').order_by('gathering__meet', '-gathering__start', 'character__display_order')
-        logger.info("27 The value of context is %s", context)
-        logger.info("28 The value of response_kwargs is %s", response_kwargs)
-        logger.info("29 The value of params is %s", params)
-        # logger.info("29 The value of request.GET('hi') is %s", 5)
+        # logger.info("27 The value of context is %s", context)
+        # logger.info("28 The value of response_kwargs is %s", response_kwargs)
+        # logger.info("29 The value of params is %s", path_params)
+        logger.info("30 The value of self.request.GET.get('hi') (search params) is %s", self.request.GET.get('hi'))
+        logger.info("31 The value of self.kwargs (url params) is %s", self.kwargs)
         return JsonResponse(
             serializers.serialize('json', participations),
             safe=False,
@@ -41,11 +44,12 @@ class ChildrenMinistryParticipationLeaderListView(ListView, ParticipationJSONRes
     template_name = 'occasions/children_ministry/participations/leader_index.html'
 
     def render_to_response(self, context, **kwargs):
-        logger.info("44 The value of kwargs is %s", kwargs)
-        logger.info("45 The value of self.request is %s", self.request)
-        logger.info("46 The value of self.kwargs is %s", self.kwargs)
+        # logger.info("45 The value of kwargs is %s", kwargs)
+        # logger.info("46 The value of self.request is %s", self.request)
+        # logger.info("47 The value of self.kwargs is %s", self.kwargs)
+        # logger.info("48 The value of self.request.GET.get('hi', '') is %s", self.request.GET.get('hi', ''))
         if self.request.is_ajax():
-            return self.render_to_json_response(context, params=self.kwargs, **kwargs)
+            return self.render_to_json_response(context, **kwargs)
         else:
             return super().render_to_response(context, **kwargs)
 
--- attendees/static/js/shared/base.js
+++ attendees/static/js/shared/base.js
@@ -1,6 +1,6 @@
-(($, Attendee) => {
+(($, Attendees) => {
   if (typeof Attendees === 'undefined') window.Attendees = {};
   console.log("attendees/static/js/shared/base.js");
   const timeZoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
   document.cookie = 'timezone=' + encodeURIComponent(timeZoneName) + '; path=/';
-})(window.jQuery, window.Attendee); // https://stackoverflow.com/a/18315393
+})(window.jQuery, window.Attendees); // https://stackoverflow.com/a/18315393
