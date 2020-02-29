781fa9846d7473326005883ae66b95202f943600
Jack Lin <jlin@castlighthealth.com>
2020-02-29 11:29:32 -0800

    remove excess data and reformat, prepare to provide data from server

--- attendees/static/js/children_ministry/participations/index.js
+++ attendees/static/js/children_ministry/participations/index.js
@@ -22,8 +22,7 @@ Attendees.leaderIndex = {
       {
         dataField: "gathering",
         lookup: {
-//          dataSource: "/1_cfcc-hayward/occasions/api/gatherings",
-          dataSource: [{"id":1,"created":"2020-02-08T15:23:16.982000-08:00","modified":"2020-02-08T15:23:16.982000-08:00","meet":1,"start":"2020-02-09T10:00:00-08:00","finish":"2020-02-09T12:00:00-08:00","display_name":"02/09/2020","link":null,"occurrence":null,"site_type":31,"site_id":1},{"id":2,"created":"2020-02-16T20:01:15.902000-08:00","modified":"2020-02-16T20:01:15.902000-08:00","meet":1,"start":"2020-02-16T10:00:00-08:00","finish":"2020-02-16T12:00:00-08:00","display_name":"02/16/2020","link":null,"occurrence":null,"site_type":32,"site_id":1},{"id":4,"created":"2020-02-17T07:09:03.335000-08:00","modified":"2020-02-17T07:09:03.335000-08:00","meet":2,"start":"2020-02-09T10:00:00-08:00","finish":"2020-02-09T12:00:00-08:00","display_name":"02/09/2020","link":null,"occurrence":null,"site_type":30,"site_id":1},{"id":3,"created":"2020-02-17T07:01:37.575000-08:00","modified":"2020-02-17T07:01:37.575000-08:00","meet":3,"start":"2020-01-10T09:00:00-08:00","finish":"2020-01-10T12:00:00-08:00","display_name":"01/10/2020","link":null,"occurrence":null,"site_type":30,"site_id":2},{"id":5,"created":"2020-02-17T18:32:54.537000-08:00","modified":"2020-02-17T18:37:42.317000-08:00","meet":4,"start":"2020-02-17T00:00:00-08:00","finish":"9999-12-29T00:00:01-08:00","display_name":"forever","link":null,"occurrence":null,"site_type":29,"site_id":2}],
+          dataSource: "/1_cfcc-hayward/occasions/api/gatherings",
           displayExpr: "display_name",
           valueExpr: "id",
         }
