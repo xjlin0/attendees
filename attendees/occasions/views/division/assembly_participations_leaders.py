f02347bd0f4828313175c527539c1361820a6b0c
Jack Lin <jlin@castlighthealth.com>
2020-03-01 19:10:06 -0800

    rename template folders

--- attendees/occasions/views/division/assembly_participations_leaders.py
+++ attendees/occasions/views/division/assembly_participations_leaders.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 @method_decorator([login_required], name='dispatch')
 class AssemblyParticipationLeaderListView(AssemblyParticipationListView):
     queryset = []
-    template_name = 'occasions/children_ministry/participations/leader_index.html'
+    template_name = 'occasions/division/assembly/participations_leader_index.html'
 
     def get_participations(self, args):
         current_division_slug = args.get('current_division_slug')
@@ -23,7 +23,7 @@ class AssemblyParticipationLeaderListView(AssemblyParticipationListView):
         return Participation.objects.select_related('character', 'team', 'attending', 'gathering', 'attending__attendee').filter(attending__divisions__slug=current_division_slug, gathering__meet__slug__in=chosen_meet_slugs, gathering__start__gte=chosen_start, gathering__finish__lte=chosen_finish).exclude(character__slug='student').order_by('gathering__meet', '-gathering__start', 'character__display_order')
 
     def get_partial_template(self):
-        return 'occasions/children_ministry/participations/_grouped_list.html'
+        return 'occasions/division/assembly/_participations_grouped_list.html'
 
 
 assembly_participation_leader_list_view = AssemblyParticipationLeaderListView.as_view()
