8414a98162c03a4315be2dc3a17cb58e9d3705dd
Jack Lin <jlin@castlighthealth.com>
2020-03-08 22:52:35 -0700

    jQuery is not loaded

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,8 @@
 console.log("assets/js/index.js 1 in index.js!!567800000");
 (($, Attendees) => {
   if (typeof Attendees === 'undefined') window.Attendees = {};
-  console.log("attendees/static/js/shared/base.js");
+  console.log("attendees/static/js/shared/index.js 4");
   const timeZoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
   document.cookie = 'timezone=' + encodeURIComponent(timeZoneName) + '; path=/';
+  console.log('jQ version: ', window.jQuery().jquery);
 })(window.jQuery, window.Attendees); // https://stackoverflow.com/a/18315393
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const BundleTracker = require('webpack-bundle-tracker');
+const $ = require("jquery");
 
 module.exports = {
   context: __dirname,
