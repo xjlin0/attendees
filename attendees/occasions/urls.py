81c4a3558cc825b909bc831b4529e2adebe48fde
Jack Lin <jlin@castlighthealth.com>
2020-03-25 19:49:53 -0700

    Revert "make the leader list page show all meets/assemblies under a division" because different assemblies's meets/characters are still very different
    This reverts commit 74dda3bcbc0948d382a9f9055349d4f080e6f5b7.

--- attendees/occasions/urls.py
+++ attendees/occasions/urls.py
@@ -14,14 +14,14 @@ from attendees.occasions.views import (
 app_name = "occasions"
 
 router = routers.DefaultRouter()
-router.register('api/(?P<division_slug>.+)/participations', api_participation_viewset, basename='participation')
+router.register('api/(?P<division_slug>.+)/(?P<assembly_slug>.+)/participations', api_participation_viewset, basename='participation')
 router.register('api/characters', api_character_viewset)
 router.register('api/teams', api_team_viewset)
 router.register('api/gatherings', api_gathering_viewset)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path("<slug:division_slug>/participations/leaders/", view=assembly_participation_leader_list_view, name="children_ministry_participations_leaders"),
-    path("<slug:division_slug>/participations/students/", view=assembly_participation_student_list_view, name="children_ministry_participations_students"),
+    path("<slug:division_slug>/<slug:assembly_slug>/participations/leaders/", view=assembly_participation_leader_list_view, name="children_ministry_participations_leaders"),
+    path("<slug:division_slug>/<slug:assembly_slug>/participations/students/", view=assembly_participation_student_list_view, name="children_ministry_participations_students"),
 
 ]
