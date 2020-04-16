a03b75a9296a8dc7a47f0d1055c2484992e52e8c
Jack Lin <jlin@castlighthealth.com>
2020-04-16 04:57:15 -0700

    testing dynamic menus in About.html works

--- attendees/context_processors.py
+++ attendees/context_processors.py
@@ -3,12 +3,14 @@ from datetime import datetime
 from django.conf import settings
 from urllib import parse
 
+from attendees.users.models import Menu
+
 
 def common_variables(request):  # TODO move organization info to view
     tzname = request.COOKIES.get('timezone') or settings.CLIENT_DEFAULT_TIME_ZONE
     user_organization_name = settings.PROJECT_NAME
     user_organization_name_slug = '0_organization_slug'
-    #user_menus = Menu.objects.filter(auth_groups__in=current_user.groups.all())
+    user_menus = Menu.objects.filter(auth_groups__in=request.user.groups.all()).distinct()
     if request.user.is_authenticated and request.user.organization:
         user_organization = request.user.organization
         user_organization_name = user_organization.display_name
@@ -17,4 +19,5 @@ def common_variables(request):  # TODO move organization info to view
         'timezone_name': datetime.now(timezone(parse.unquote(tzname))).tzname(),
         'user_organization_name': user_organization_name,
         'user_organization_name_slug': user_organization_name_slug,
-    }  # Todo pass in user_division_slugs based on users' auth_group to display menu
+        'user_menus': user_menus,
+    }
