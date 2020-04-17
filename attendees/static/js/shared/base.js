34e92a50517b85a42d86e1abf873c0c02ea7d761
Jack Lin <jlin@castlighthealth.com>
2020-04-17 08:10:45 -0700

    make bootstrap nav active state work for every page

--- attendees/static/js/shared/base.js
+++ attendees/static/js/shared/base.js
@@ -3,4 +3,7 @@
   console.log("attendees/static/js/shared/base.js");
   const timeZoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
   document.cookie = 'timezone=' + encodeURIComponent(timeZoneName) + '; path=/';
+
+  // $('li.active').removeClass('active');
+  $('a[href="' + location.pathname + location.search + '"]').closest('li').addClass('active');
 })(window.jQuery, window.Attendees); // https://stackoverflow.com/a/18315393
