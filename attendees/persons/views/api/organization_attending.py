557ff342e68fbb739adff112db63270fa75bdc9d
Jack Lin <jlin@castlighthealth.com>
2020-04-18 17:40:26 -0700

    add some doc for the complicate organization attending query

--- attendees/persons/views/api/organization_attending.py
+++ attendees/persons/views/api/organization_attending.py
@@ -18,11 +18,18 @@ class ApiOrganizationAttendingViewSet(viewsets.ModelViewSet):
     serializer_class = AttendingSerializer
 
     def get_queryset(self):
+        """
+        :permission: this API is only for coworkers/organizers, ordinary participants should get nothing from this API
+        :query: Find all gatherings of the current user, then list all attendings of the found gatherings.
+                So if the current user didn't participate(attending), no info will be shown
+        :return: all Attendings with participating meets(group) and character(role)
+        """
         current_user = self.request.user
         if current_user.belongs_to_organization_of(self.kwargs['organization_slug']):
             meets = self.request.query_params.getlist('meets[]', [])
             user_attended_gathering_ids = current_user.attendee.attendings.values_list('gathering__id', flat=True).distinct()
             return Attending.objects.select_related().prefetch_related().filter(
+                #registration_start/finish within the selected time period.
                 meets__slug__in=meets,
                 gathering__id__in=user_attended_gathering_ids,
                 meets__assembly__division__organization__slug=self.kwargs['organization_slug'],
